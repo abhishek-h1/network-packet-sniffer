0ce8ee4294de699cc994b7609cad4c9832685bb7
Priyanka Srinivas <priyasrini55@gmail.com>
2021-11-13 00:04:53 +0530

    Create sniffer.py
    - Connected the socket
    - Added ethernet packet unwrapping
    - Formatted MAC address

---
+++ sniffer.py
@@ -0,0 +1,33 @@
+import socket
+import struct
+import textwrap
+
+
+# Create socket connection to run the program indefinitely
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print("\nEthernet frame:")
+        print("Destination: {}, Source: {}, Protocol: {}".format(dest_mac, src_mac, eth_proto))
+
+
+# Unpack ethernet frame to find the data
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+
+# Return properly formatted MAC address (i.e. AA:BB:CC:DD:EE:FF)
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).upper()
+    return mac_addr
+
+# Unpack IPv4 packet
+
+
+if __name__ == '__main__':
+    main()
